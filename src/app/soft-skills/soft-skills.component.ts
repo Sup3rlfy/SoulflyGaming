525d97d1b1d0c2f1a141bfcbc3f116104bed2e91
Sup3rlfy <64333638+Sup3rlfy@users.noreply.github.com>
2025-05-19 07:02:31 +0200

    soft SkillZ!

---
+++ src/app/soft-skills/soft-skills.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soft-skills',
+  imports: [],
+  templateUrl: './soft-skills.component.html',
+  styleUrl: './soft-skills.component.scss'
+})
+export class SoftSkillsComponent {
+
+}
